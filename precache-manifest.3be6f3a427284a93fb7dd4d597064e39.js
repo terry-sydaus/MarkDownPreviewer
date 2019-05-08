a6cf18932589886cf72e9cd59663699c9f2deb11
Terry Murphy <terryjmurphy71@gmail.com>
2019-05-08 18:11:18 +1000

    Updates

---
+++ precache-manifest.3be6f3a427284a93fb7dd4d597064e39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34a799c87a679d056d2",
+    "url": "/MarkDownPreviewer/static/js/runtime~main.e3107278.js"
+  },
+  {
+    "revision": "b5c2837783068aa69cf0",
+    "url": "/MarkDownPreviewer/static/js/main.74d381d4.chunk.js"
+  },
+  {
+    "revision": "7671ea1c7e3d39fb5ff3",
+    "url": "/MarkDownPreviewer/static/js/2.0ce921ef.chunk.js"
+  },
+  {
+    "revision": "b5c2837783068aa69cf0",
+    "url": "/MarkDownPreviewer/static/css/main.7dc10f7b.chunk.css"
+  },
+  {
+    "revision": "852645366812957ad2106709e75accf6",
+    "url": "/MarkDownPreviewer/index.html"
+  }
+];
